a5b55651a3d5fb8df6b0dfc7a17f4a7326599e79
Alkesh Nikalje <alkeshnikalje@gmail.com>
2023-07-06 16:59:18 +0530

    wrote server code

---
+++ server.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
+const app = express();
+
+app.use(express.json());
+
+// define schemas
+const userSchema = new mongoose.Schema({
+    username : String,
+    password : String
+})
+
+const todoSchema = new mongoose.Schema({
+    title : String,
+    description : String
+})
+
+// define models
+const Todo = mongoose.model('Todo',todoSchema);
+const User = mongoose.model('Users',userSchema);
+
+// connect to database
+
+mongoose.connect("mongodb+srv://alkeshnikalje:nrRjp3okv4Q0od16@cluster0.md9wzl6.mongodb.net/Todos",{useNewUrlParser: true,useUnifiedTopology: true});
+
+
+// secret and fucntion for authentication 
+const secret = "ThisIsASecret";
+const userAuth = (req,res,next)=>{
+    const authHeader = req.headers.authorization;
+    if(authHeader){
+        const token = authHeader.split(' ')[1];
+        jwt.verify(token,secret,(err,user)=>{
+            if(err){
+                return res.sendStatus(403);
+            }
+            req.user = user;
+            next();
+        })
+    }else{
+        res.sendStatus(401);
+    }
+};
+
+// user routes
+app.post('/users/signup',async (req,res)=>{
+    const  {username, password} = req.body;
+    const user = await User.findOne({username});
+    if(user){
+        res.status(403).json({message : "User already exists"});
+    }else{
+        const newUser = new User({username,password});
+        await newUser.save();
+        const token = jwt.sign({username,password},secret, {expiresIn : '24h'});
+        res.json({message : "user created successfully", token});
+    }
+})
+
+app.post('/users/login',async(req,res)=>{
+    const {username,password} = req.body;
+    const user = await User.findOne({username,password});
+    if(user){
+        const token = jwt.sign({username,password},secret,{expiresIn : '24h'});
+        res.json({message: "logged in successfully",token});
+    }else{
+        res.status(403).json({message: 'Invalid username or password'});
+    }
+})
+
+app.post('/users/todos',userAuth, async(req,res)=>{
+    const todo = new Todo(req.body);
+    await todo.save();
+    res.json({title : todo.title,description: todo.description,id: todo.id});
+})
+
+
+
+app.listen(3000,()=>{
+    console.log('now listening on 3000');
+})
